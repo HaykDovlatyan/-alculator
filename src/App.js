d14ae0c26f167498b9edd4a9149df925b28c9a84
HaykDovlatyan <haykdovlatyan752@gmail.com>
2024-03-10 01:46:51 +0400

    react-calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const checks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', '-', '*', '/', '.'];
+  const [result, setResult] = useState(0);
+  const concatCheck = e => {
+    result === 0 ?
+      setResult(e.target.innerText)
+      : setResult(result.toString().concat(e.target.innerText))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calc">
+        <div className="result"><span>{result}</span></div>
+        <div className="buttons">
+          <button onClick={() => {
+            setResult(0);
+          }}>AC</button>
+          <button onClick={() => {
+            if (result > 9) {
+              setResult(result.slice(0,-1))
+            }
+            if (result > 1 && result < 9)  {
+              setResult(0)
+            }
+          }}>C</button>
+          {checks.map(check => <button onClick={concatCheck} key={check}>{check}</button>)}
+          <button onClick={() => {
+            try { setResult(eval(result)) }
+            catch { setResult(0) }
+          }}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
